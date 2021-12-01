bbdc12274a4ed54c6ef2238ada3ca9c004a9125c
hoangduylq <duy.luong@zodinet.tech>
2021-12-01 17:01:57 +0700

    add login-fb

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NFT Ticketing')
+    .setDescription('API for NFT Ticketing ')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
 
   const configService = app.select(ConfigModule).get(ConfigService);
 
---
+++ src/modules/auth/strategies/facebook.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile } from 'passport-facebook';
+import { URL_APP_CLIENT } from './../../../shared/constant';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: process.env.FB_APP_ID,
+      clientSecret: process.env.FB_APP_SECRET,
+      callbackURL: `${URL_APP_CLIENT}facebook/redirect`,
+      scope: 'email',
+      profileField: ['emails', 'name'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
