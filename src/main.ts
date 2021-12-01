1b8fd1c615ce4a03cf1d902b4c171a431407403a
hoangduylq <duy.luong@zodinet.tech>
2021-12-01 11:58:51 +0700

    update enableCors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,17 @@ async function bootstrap() {
 
   const configService = app.select(ConfigModule).get(ConfigService);
 
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   await app.listen(configService.get('port'));
 }
