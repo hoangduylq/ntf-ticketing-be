aa0fd5688ac9be6633200ae4de61ddffe4fa81f1
hoangduylq <duy.luong@zodinet.tech>
2021-12-10 13:14:06 +0700

    combined event api

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -1,10 +1,10 @@
-import { TicketModule } from './../ticket/ticket.module';
 import { Module } from '@nestjs/common';
-import { PaymentController } from './controllers/payment.controller';
-import { PaymentService } from './services/payment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderRepository } from './infrastructure/order.repository';
+import { TicketModule } from './../ticket/ticket.module';
+import { PaymentService } from './services/payment.service';
+import { PaymentController } from './controllers/payment.controller';
 import { EventModule } from '../event/event.module';
+import { OrderRepository } from './infrastructure/order.repository';
 
 @Module({
   imports: [
